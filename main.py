d4f92ae9589b1c133281b8b0c91e9e1244dcd9bf
EvanDaley <daley.evan@gmail.com>
2024-12-22 13:36:04 -0800

    First functional test

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 import sys
-
 from dotenv import load_dotenv
-
-from langchain.agents import load_tools
+from langchain_openai import OpenAI
 from langchain.agents import initialize_agent
-from langchain.llms import OpenAI
 
 from commands import chrome_click_on_link, chrome_get_the_links_on_the_page, chrome_open_url, chrome_read_the_page, computer_applescript_action, say_text
 
-
-# load environment variables
+# Load environment variables
 load_dotenv()
 
 def main(command):
-    llm = OpenAI(temperature=0)
+    llm = OpenAI(temperature=0)  # Ensure API key is set in .env or passed directly
 
     tools = [
         computer_applescript_action,
         chrome_open_url,
         chrome_get_the_links_on_the_page,
         chrome_click_on_link,
-        chrome_read_the_page
+        chrome_read_the_page,
     ]
 
     agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
